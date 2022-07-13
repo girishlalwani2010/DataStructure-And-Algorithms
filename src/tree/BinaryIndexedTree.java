f16f3cd9fa94f0f9a97ea007136a66e53a132d94
girishlalwani2010 <girishlalwani2010@gmail.com>
2022-07-14 02:35:41 +0530

    Create BinaryIndexedTree.java
    added Binary Indexed Tree array representation code example.

---
+++ src/tree/BinaryIndexedTree.java
@@ -0,0 +1,54 @@
+// Also called to be Fenwick-Tree
+public class BinaryIndexedTree {
+    
+    private int[] tree;
+    
+    public void initialize(int []input) {
+      tree = new int[input.length+1];
+      for(int i=0; i<input.length; i++) {
+         update(i+1, input[i]);
+      }  
+    }
+  
+    // add val to index of BIT  
+    public void update(int idx, int val) {
+        for(; idx<tree.length; idx = getNext(idx)){
+          tree[idx]+=val;
+        }
+    }
+  
+    //return sum from 0 to idx
+    public int getSum(int idx) {
+      int sum=0;
+      for(; idx>0; idx=getParent(idx)){
+        sum+=tree[idx];
+      }
+      return sum;
+    }  
+  
+    private int getNext(int idx) {
+        idx += idx&-idx;
+        return idx;
+    }
+  
+    private int getParent(int idx) {
+        idx -= idx&-idx;
+        return idx;
+    }
+      
+    //test BIT operations
+    public static void main(String args[]) {
+        int[] input = {3,7,1,2,10};
+        BinaryIndexedTree binaryIndexedTree = new BinaryIndexedTree();
+        binaryIndexedTree.initialize(input);
+        //get sum from L to R = getSum(R) - getSum(L-1), here we are printing getSum from 4 to 5.
+        System.out.println(binaryIndexedTree.getSum(5)-binaryIndexedTree.getSum(3));
+        binaryIndexedTree.update(4,10);
+        binaryIndexedTree.update(5,15);
+        System.out.println(binaryIndexedTree.getSum(5)-binaryIndexedTree.getSum(3));
+
+    } 
+
+
+
+}
